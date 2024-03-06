8c1e7df69c39355ffa1d5d2db86ea176ed128b0a
Arthur_Ft <arthutfreztedardi@gmail.com>
2024-03-05 23:43:41 -0300

    05/03/24
    23:43

--- Exercícios/ex029.py
+++ Exercícios/ex029.py
@@ -1,5 +1,5 @@
 velo = float(input("Qual é a velocidade atual do carro? "))
 if velo > 80:
-    print("MULTADO! Voce excedeu o limite de velocidade que é de 80Km/h")
-    print(f"Voce deve pagar uma multa de R${(velo-80) * 7:.2f}")
-print("Tenha um bom dia! Dirija com cuidado!")
+    print("\033[31mMULTADO! Voce excedeu o limite de velocidade que é de 80Km/h")
+    print(f"Voce deve pagar uma multa de \033[33mR${(velo-80) * 7:.2f}")
+print("\033[32mTenha um bom dia! Dirija com segurança!\033[m")
