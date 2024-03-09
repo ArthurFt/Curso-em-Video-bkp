9162f972deb0417a9387ccfa2b8f0b17d979c71a
Arthur_Ft <arthutfreztedardi@gmail.com>
2024-03-09 00:21:46 -0300

    09/03/24
    00:21

--- Exercícios/ex039.py
+++ Exercícios/ex039.py
@@ -1,11 +1,11 @@
 from datetime import date 
-
 ano = int(input("Em que ano voce nasceu? "))
 idade = date.today().year - ano
+print(f"Quem nasceu em {ano} tem {idade} anos em {date.today().year}.")
 
 if idade < 18:
-    print(f"\033[33mFaltam {18 - idade} anos para voce se alistar ao serviço militar!\033[m")
+    print(f"\033[33mFaltam {18 - idade} anos para voce se alistar ao serviço militar!\nSeu alistamento será em {date.today().year + (18 - idade)} \033[m")
 elif idade == 18:
     print(f"\033[32mPARABÉNS! Está na hora de voce fazer o seu ALISTAMENTO ao serviço militar!\033[m")
 elif idade > 18:
-    print(f"\033[31mOpa... Voce deveria ter feito seu alistamento a \033[33m{idade - 18}\033[31m anos atras.\033[m")
+    print(f"\033[31mOpa... Voce deveria ter feito seu alistamento a \033[33m{idade - 18}\033[31m anos atras.\nSeu alistamento foi em {date.today().year - (idade - 18  )} \033[m")
